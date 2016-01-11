5acb57565e418d16def67e11914c57220d1daad5
unknown <kagnihotri@Crux.internal.tavisca.com>
2016-01-11 11:28:39 +0530

    readonlylist

---
+++ ReadOnlyCollectionInNet4.5/DictionaryFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyCollectionInNet4._5
+{
+    public class DictionaryFactory
+    {
+
+        public Dictionary<string, int> _Configs;
+        public DictionaryFactory()
+        {
+            _Configs = new Dictionary<string, int>();
+            _Configs.Add("key", 1);
+            _Configs.Add("key1", 2);
+            _Configs["key2"] = 2;
+        }
+        public ReadOnlyDictionary<string, int> Configuration
+        {
+            get
+            {
+                return new ReadOnlyDictionary<string, int>(_Configs); 
+            }
+        }
+
+    }
+}
---
+++ ReadOnlyCollectionInNet4.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyCollectionInNet4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DictionaryFactory obj = new DictionaryFactory();
+                obj._Configs.Add("temp", 3);
+                var x = obj.Configuration;
+                Console.WriteLine("Readonly dictionary used to ensure the data is not modified across the app while passing data ");
+                foreach (var item in x)
+                {
+                    Console.WriteLine(item.Key + " -- "+ item.Value);
+                }
+            }
+            catch (Exception e)
+            {
+                
+                throw;
+            }
+           
+        }
+    }
+}
